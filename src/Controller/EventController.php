fdafd978ff8fc311a6cb28a381a47c596d9c22e4
SimDbs <38361526+SimDbs@users.noreply.github.com>
2021-01-20 18:53:17 +0100

    Apprends

---
+++ src/Controller/EventController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Event;
+use App\Form\EventType;
+use App\Repository\EventRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/event", name="event")
+     */
+    public function index(EventRepository $event): Response
+    {
+        $events = $event->findAll();
+
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+            'events' => $events
+        ]);
+    }
+
+    /**
+     * @Route("/event/create", name="event_create")
+     */
+    public function create(Event $event = null, Request $request, ObjectManager $objectManager): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $event->setDateCreation(new \DateTime());
+            $event->setDateModification(new \DateTime());
+
+            $objectManager->persist($event);
+            $objectManager->flush();
+        }
+
+        return $this->render('event/create.html.twig', [
+            'controller_name' => 'EventController',
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Entity/Event.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventRepository::class)
+ */
+class Event
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_creation;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_modification;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_suppression;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbParticipant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getNbParticipant(): ?int
+    {
+        return $this->nbParticipant;
+    }
+
+    public function setNbParticipant(int $nbParticipant): self
+    {
+        $this->nbParticipant = $nbParticipant;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreation()
+    {
+        return $this->date_creation;
+    }
+
+    /**
+     * @param mixed $date_creation
+     */
+    public function setDateCreation($date_creation): void
+    {
+        $this->date_creation = $date_creation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateModification()
+    {
+        return $this->date_modification;
+    }
+
+    /**
+     * @param mixed $date_modification
+     */
+    public function setDateModification($date_modification): void
+    {
+        $this->date_modification = $date_modification;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateSuppression()
+    {
+        return $this->date_suppression;
+    }
+
+    /**
+     * @param mixed $date_suppression
+     */
+    public function setDateSuppression($date_suppression): void
+    {
+        $this->date_suppression = $date_suppression;
+    }
+
+}
