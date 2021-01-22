01d12f770154543813620dcedcff5af63029e30b
SimDbs <38361526+SimDbs@users.noreply.github.com>
2021-01-22 13:56:44 +0100

    event page design

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/navbar.css';
+import './styles/userNavbar.css';
+import './styles/events.css';
 
 // start the Stimulus application
 const $ = require('jquery');
