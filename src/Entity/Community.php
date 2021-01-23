d7cc6f2dc79157faffb31803db348d23e548f52b
laugere <martin.laugere@youday.fr>
2021-01-23 17:00:56 +0100

    adding community

---
+++ src/Entity/Community.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommunityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommunityRepository::class)
+ */
+class Community
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_creation;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_modification;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date_suppression;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateCreation()
+    {
+        return $this->date_creation;
+    }
+
+    /**
+     * @param mixed $date_creation
+     */
+    public function setDateCreation($date_creation): void
+    {
+        $this->date_creation = $date_creation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateModification()
+    {
+        return $this->date_modification;
+    }
+
+    /**
+     * @param mixed $date_modification
+     */
+    public function setDateModification($date_modification): void
+    {
+        $this->date_modification = $date_modification;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateSuppression()
+    {
+        return $this->date_suppression;
+    }
+
+    /**
+     * @param mixed $date_suppression
+     */
+    public function setDateSuppression($date_suppression): void
+    {
+        $this->date_suppression = $date_suppression;
+    }
+}
