4e0c4184979448649bd65d6331d76d08e8b6a5ab
laugere <martinlaugere37@gmail.com>
2021-03-18 19:20:18 +0100

    start work search

--- src/Controller/HomePage.php
+++ src/Controller/HomePage.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Participation;
+use App\Repository\CommunityRepository;
 use App\Repository\EventRepository;
+use App\Repository\FriendshipRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\EventRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomePage extends AbstractController
@@ -27,6 +31,25 @@ class HomePage extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'events' => $events,
+            'menu' => 'home'
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="search")
+     */
+    public function search(UserRepository $userRepo, EventRepository $eventRepo, CommunityRepository $communityRepo, FriendshipRepository $friendshipRepo, Request $request): Response
+    {
+        $users = $userRepo->findByLike($request->query->get('search'));
+        $events = $eventRepo->findByLike($request->query->get('search'));
+        $communitys = $communityRepo->findByLike($request->query->get('search'));
+        $friendships = $friendshipRepo->findAll();
+
+        return $this->render('search/index.html.twig', [
+            'users' => $users,
+            'events' => $events,
+            'communitys' => $communitys,
+            'friendships' => $friendships,
             'menu' => 'home'
         ]);
     }
